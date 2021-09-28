82b3bae31da13cf378a8b875bc4c3b6b6c1c49f4
Brian-Mulei <brianmulei0@gmail.com>
2021-09-28 23:18:58 +0300

    Update

--- app/src/main/java/com/mulei/caloriecounter/MainActivity.java
+++ app/src/main/java/com/mulei/caloriecounter/MainActivity.java
@@ -1,14 +1,44 @@
 package com.mulei.caloriecounter;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+    SignInButton google_button;
+    GoogleSignInClient mGoogleSignInClient;
+    int RC_SIGN_IN =0 ;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // FirebaseApp.initializeApp(this);
+
     }
+
 }
